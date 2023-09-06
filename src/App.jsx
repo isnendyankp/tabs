24c9c75970da5a5cee756e3a9fbbf04d8409fff3
isnendyankp <isnendyankp@gmail.com>
2023-09-06 10:01:26 +0700

    S7-203:cr8 jobs state

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ const url = 'https://course-api.com/react-tabs-project';
 
 const App = () => {
   const [isloading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
   return <h2>Tabs Starter</h2>;
 };
 export default App;
@@ -9,3 +10,4 @@ export default App;
 
 // - S7-202:Setup Tabs Project
 // - S7-203:cr8 isLoading state equal true
+// - S7-203:cr8 jobs state equal empty array
