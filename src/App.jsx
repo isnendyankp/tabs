b0acd5e9da464e1c3a56bfb0b910797e9ecab24d
isnendyankp <isnendyankp@gmail.com>
2023-09-06 09:59:37 +0700

    S7-203:cr8 isLoading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 const url = 'https://course-api.com/react-tabs-project';
 
 const App = () => {
+  const [isloading, setIsLoading] = useState(true);
   return <h2>Tabs Starter</h2>;
 };
 export default App;
 
 
 // - S7-202:Setup Tabs Project
+// - S7-203:cr8 isLoading state equal true
