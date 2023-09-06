d1972084a180b5ed11eedb1315022e2c44af6ed9
isnendyankp <isnendyankp@gmail.com>
2023-09-06 10:06:30 +0700

    S7-203:cr8 fetchJobs function for retrieve data

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ const url = 'https://course-api.com/react-tabs-project';
 const App = () => {
   const [isloading, setIsLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async() =>{
+
+  }
   return <h2>Tabs Starter</h2>;
 };
 export default App;
@@ -11,3 +15,4 @@ export default App;
 // - S7-202:Setup Tabs Project
 // - S7-203:cr8 isLoading state equal true
 // - S7-203:cr8 jobs state equal empty array
+// - S7-203:cr8 fetchJobs function for retrieve data from API with asynchronously
