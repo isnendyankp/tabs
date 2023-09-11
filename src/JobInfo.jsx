0ce5bc53352e82f6464edbfc75de8b266ee4f442
isnendyankp <isnendyankp@gmail.com>
2023-09-11 09:26:47 +0700

    S7-204:Add article with CN job-info

--- src/JobInfo.jsx
+++ src/JobInfo.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const JobInfo = ({jobs}) => {
   return (
-    <div>JobInfo</div>
+    <article className='job-info'>
+
+    </article>
   )
 }
 
@@ -10,3 +12,4 @@ export default JobInfo
 
 // - S7-204:Cr8 base JobInfo component
 // - S7-204:Pass in parameter jobs
+// - S7-204:Add article with CN job-info
