b12a6685d6a4dccec3e18fbfc802c7eacbb839fd
isnendyankp <isnendyankp@gmail.com>
2023-09-12 08:58:10 +0700

    S7-204:Checking log after acces duties at Duties component

--- src/Duties.jsx
+++ src/Duties.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 const Duties = ({duties}) => {
-  return (
+    console.log(duties);
+    return (
     <div>Duties</div>
   )
 }
@@ -10,3 +11,4 @@ export default Duties
 
 // - S7-204:cr8 Duties component base
 // - S7-204:Accessing duties on Duties component
+// - S7-204:Checking log after acces duties at Duties component
