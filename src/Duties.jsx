f60e8161c27c62388ec2a99717510c60c2ffdd60
isnendyankp <isnendyankp@gmail.com>
2023-09-13 08:33:36 +0700

    S7-205:import react icons double right

--- src/Duties.jsx
+++ src/Duties.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa';
 
 const Duties = ({duties}) => {
     console.log(duties);
@@ -12,3 +13,4 @@ export default Duties
 // - S7-204:cr8 Duties component base
 // - S7-204:Accessing duties on Duties component
 // - S7-204:Checking log after acces duties at Duties component
+// - S7-205:import react icons double right
