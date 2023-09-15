f787bfec6b33a4fd3ebc1fff7b5c36fefd39f6cd
isnendyankp <isnendyankp@gmail.com>
2023-09-15 13:42:13 +0700

    S7-206: Add return with button @callback function

--- src/BtnContainer.jsx
+++ src/BtnContainer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+
 const BtnContainer = ({jobs}) => {
   return (
     <div className='btn-container'>
-        {jobs.map(()=>)}
+        {jobs.map(()=>{
+            return <button />
+        })}
     </div>
   )
 }
@@ -14,3 +17,4 @@ export default BtnContainer
 // - S7-206:Accessing jobs @BtnContainer component
 // - S7-206: Add CN btn-container @return div
 // - S7-206: Add jobs.map with callback function @return div
+// - S7-206: Add return with button @callback function
